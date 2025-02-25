e5bc2e4215357c5602aa86be1baf413ab08badb5
Kadyr Gullyyev <kadyr.gullyyew@gmail.com>
2025-02-25 11:52:27 -0800

    Delete delivery directory

--- delivery/catalogues/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CataloguesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'catalogues'
-
-    def ready(self):
-        import catalogues.signals
