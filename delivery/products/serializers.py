acb6713f076eeece325c9cc3af0724f5c55dd3b9
Kadyr Gullyyev <kadyr.gullyyew@gmail.com>
2025-02-28 10:43:14 -0800

    commit

--- delivery/products/serializers.py
+++ delivery/products/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from .models import Product
 
-
 class ProductSerializer(serializers.ModelSerializer):
     title = serializers.SerializerMethodField()
     description = serializers.SerializerMethodField()
     image = serializers.SerializerMethodField()
+    restaurant = serializers.SerializerMethodField()
     
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description', 'image', 'initial_price', 'discount', 'discount_state', 'price', 'slug']
+        fields = ['id', 'title', 'description', 'image', 'initial_price', 'discount', 'discount_state', 'restaurant', 'price', 'slug']
 
     def get_title(self, obj):
         request = self.context.get('request')
@@ -20,7 +20,10 @@ class ProductSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         lang = request.headers.get('Accept-Language', 'tm') if request else 'tm'
         return getattr(obj, f'description_{lang}', obj.description_tm)
-
+    
+    def get_restaurant(self, obj):
+        return obj.slug
+    
     def get_image(self, obj):
         request = self.context.get('request')
         view = self.context.get('view')
