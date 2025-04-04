8c228a6335805434c83f08b74fa95725a4c813ee
Kadyr Gullyyev <kadyr.gullyyew@gmail.com>
2025-04-04 11:36:55 -0700

    commit

--- delivery/accounts/views.py
+++ delivery/accounts/views.py
@@ -8,7 +8,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from .permissions import IsSuperUser, IsAdminOrSuperUser
 from .models import User, Code
-from .serializers import UserSerializer, ProfileSerializer, AuthSerializer
+from .serializers import UserSerializer, ProfileSerializer, AuthSerializer, LoginSerializer, VerifySerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -73,6 +73,54 @@ class UserViewSet(viewsets.ModelViewSet):
                         return Response({'error': "Something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
         if request.method == "GET":
             return Response()
+
+
+    @action(methods=['post'], detail=False, url_path='login')
+    def login(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        email = request.data.get('email')
+
+        if serializer.is_valid():
+            try:
+                code = get_random_string(length=4, allowed_chars='0123456789')
+                user, created = User.objects.get_or_create(email=email)
+                Code.objects.create(user=user, code=code)
+                send_mail(
+                    subject='Your OTP Code',
+                    message=f'Your OTP code is {code}',
+                    from_email='your_email@example.com',
+                    recipient_list=[email],
+                    fail_silently=False,
+                )
+                return Response({'email': email}, status=status.HTTP_200_OK)
+            except:
+                return Response({'error': "Something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
+                
+    @action(methods=['post'], detail=False, url_path='verify')
+    def verify(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        email = request.data.get('email')
+        code = request.data.get('code')
+        user = User.objects.filter(email=email).first()
+        if serializer.is_valid:
+            if not user:
+                return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+            code_instance = Code.objects.filter(user=user, code=code).first()
+            if code_instance.is_valid():
+                try:
+                    password = get_random_string(length=8, allowed_chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+                    user.set_password(password)
+                    user.save()
+                    user = authenticate(email=email, password=password)
+                    access_token = RefreshToken.for_user(user).access_token
+                    return Response({'access': str(access_token)}, status=status.HTTP_200_OK)
+                except:
+                    return Response({'error': "Something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
+            elif not code_instance.is_valid():
+                return Response({'message': 'Code is expired'}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response({'message': 'Code not found'}, status=status.HTTP_404_NOT_FOUND)
+
     def get_queryset(self):
         if self.request.user.type in [User.UserType.ADMIN]:
             return User.objects.exclude(type = User.UserType.SUPERUSER).order_by('date_joined')
@@ -81,7 +129,7 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action == "destroy":
             return [IsSuperUser()]
-        if self.action == "auth":
+        if self.action == "login" or "verify":
             return [AllowAny()]
         if self.action == "profile":
             return [IsAuthenticated()]
@@ -92,4 +140,8 @@ class UserViewSet(viewsets.ModelViewSet):
             return ProfileSerializer
         if self.action == "auth":
             return AuthSerializer
+        if self.action == "login":
+            return LoginSerializer
+        if self.action == "verify":
+            return VerifySerializer
         return UserSerializer
