f4bcc4c1afc43a4ead2da3c50f772ece7c1b1435
Kadyr Gullyyev <kadyr.gullyyew@gmail.com>
2025-04-04 14:19:15 -0700

    commit

--- delivery/orders/serializers.py
+++ delivery/orders/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, OrderItem
+from products.serializers import ProductSerializer
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'quantity', 'total_price']
+
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    order_item = OrderItemSerializer(many=True)
+    
     class Meta:
         model = Order
-        fields = ['id', 'user']
+        fields = ['id', 'total_price', 'order_item']
