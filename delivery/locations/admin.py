957dea8821de3efcc2c253a0573d9fd8b1afd5de
Kadyr Gullyyev <kadyr.gullyyew@gmail.com>
2025-04-05 07:56:18 -0700

    commit

--- delivery/locations/admin.py
+++ delivery/locations/admin.py
@@ -1,22 +1,10 @@
 from django.contrib import admin
-from .models import Location, Address
+from .models import Location
 
-
-class AddressLineInline(admin.TabularInline):
-    model = Address
-    extra = 1
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ["id", "title_tm", "title_ru", 'queue']
     list_editable = ["title_tm", "title_ru", 'queue']
     ordering = ['queue']
-    inlines = [AddressLineInline]
 
 admin.site.register(Location, LocationAdmin)
-
-
-class AddressAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "location"]
-    list_editable = ["title", "location"]
-    list_filter = ('location',)
-admin.site.register(Address, AddressAdmin)
